76c7bf629418dc2124b59805e6c3c9662a1c87f2
han0911 <a01099249559@gmail.com>
2026-09-07 10:46:08 +0900

    fix: assign p2 reverse technique control

--- jjk-game/src/game/systems/InputManager.ts
+++ jjk-game/src/game/systems/InputManager.ts
@@ -6,8 +6,8 @@ export interface InputSnapshot { left: boolean; right: boolean; jumpPressed: boo
 type InputLayout = 'solo' | 'versus'
 
 const bindings = {
-  P1: { left: 'A', right: 'D', jump: 'W', guard: 'F', attack: 'F', strong: 'G', simpleDomain: 'G', skill1: 'E', skill2: 'R', skill3: 'T', skill4: 'Y', skill5: 'U', ultimate: 'H' },
-  P2: { left: 'LEFT', right: 'RIGHT', jump: 'UP', guard: 'TWO', attack: 'ONE', strong: 'K', simpleDomain: 'THREE', skill1: 'FOUR', skill2: 'FIVE', skill3: 'SIX', skill4: 'SEVEN', skill5: 'EIGHT', ultimate: 'L' },
+  P1: { left: 'A', right: 'D', jump: 'W', guard: 'F', attack: 'F', strong: 'G', simpleDomain: 'G', reverse: 'NINE', skill1: 'E', skill2: 'R', skill3: 'T', skill4: 'Y', skill5: 'U', ultimate: 'H' },
+  P2: { left: 'LEFT', right: 'RIGHT', jump: 'UP', guard: 'TWO', attack: 'ONE', strong: 'K', simpleDomain: 'THREE', reverse: 'NINE', skill1: 'FOUR', skill2: 'FIVE', skill3: 'SIX', skill4: 'SEVEN', skill5: 'EIGHT', ultimate: 'L' },
 } as const
 const soloP1Bindings = bindings.P1
 
@@ -27,7 +27,7 @@ export class InputManager {
     const keyboard = scene.input.keyboard
     if (!keyboard) throw new Error('Keyboard input is unavailable')
     const map = layout === 'solo' && slot === 'P1' ? soloP1Bindings : bindings[slot]
-    this.keys = keyboard.addKeys(`${map.left},${map.right},${map.jump},${map.guard},${map.attack},${map.strong},${map.simpleDomain},${map.skill1},${map.skill2},${map.skill3},${map.skill4},${map.skill5},${map.ultimate}`) as Record<string, Phaser.Input.Keyboard.Key>
+    this.keys = keyboard.addKeys(`${map.left},${map.right},${map.jump},${map.guard},${map.attack},${map.strong},${map.simpleDomain},${map.reverse},${map.skill1},${map.skill2},${map.skill3},${map.skill4},${map.skill5},${map.ultimate}`) as Record<string, Phaser.Input.Keyboard.Key>
     if (layout === 'solo' && slot === 'P1') {
       scene.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => { if (pointer.button === 0) this.mouseAttackPressed = true; if (pointer.button === 2) this.mouseReversePressed = true })
       scene.input.on('pointermove', (pointer: Phaser.Input.Pointer) => { this.mouseX = pointer.worldX })
@@ -44,7 +44,7 @@ export class InputManager {
     if (leftKey.isDown) this.lastLeftAt = now
     if (rightKey.isDown) this.lastRightAt = now
     const attackPressed = this.layout === 'solo' && this.slot === 'P1' ? this.mouseAttackPressed : Phaser.Input.Keyboard.JustDown(this.keys[map.attack])
-    const reversePressed = this.layout === 'solo' && this.slot === 'P1' ? this.mouseReversePressed : false
+    const reversePressed = this.layout === 'solo' && this.slot === 'P1' ? this.mouseReversePressed : Phaser.Input.Keyboard.JustDown(this.keys[map.reverse])
     this.mouseAttackPressed = false; this.mouseReversePressed = false
     const p1Solo = this.layout === 'solo' && this.slot === 'P1'
     return { left: leftKey.isDown, right: rightKey.isDown, jumpPressed: Phaser.Input.Keyboard.JustDown(this.keys[map.jump]), guard: this.keys[map.guard].isDown, attackPressed, reversePressed, strongPressed: p1Solo ? false : Phaser.Input.Keyboard.JustDown(this.keys[map.strong]), simpleDomainPressed: Phaser.Input.Keyboard.JustDown(this.keys[map.simpleDomain]), skill1Pressed: Phaser.Input.Keyboard.JustDown(this.keys[map.skill1]), skill2Pressed: Phaser.Input.Keyboard.JustDown(this.keys[map.skill2]), skill3Pressed: Phaser.Input.Keyboard.JustDown(this.keys[map.skill3]), skill4Pressed: Phaser.Input.Keyboard.JustDown(this.keys[map.skill4]), skill5Pressed: Phaser.Input.Keyboard.JustDown(this.keys[map.skill5]), ultimatePressed: Phaser.Input.Keyboard.JustDown(this.keys[map.ultimate]), dashLeft, dashRight, aimX: p1Solo ? this.mouseX : null }
